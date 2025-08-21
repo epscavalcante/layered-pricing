ed61af9cd878c738cb6f1d021e6e627e59d35851
Eduardo Cavalcante <eduardo.ps.cavalcante@gmail.com>
2025-08-21 19:24:29 -0400

    add new config phinx

--- phinx.php
+++ phinx.php
@@ -12,13 +12,20 @@ return
         'default_migration_table' => 'migrations',
         'default_environment' => 'default',
         'default' => [
-            'adapter' => Config::get('DB_CONNECTION','sqlite'),
-            'host' => Config::get('DB_HOST', ':memory:'),
-            'name' => Config::get('DB_DATABASE', 'pricing'),
+            'adapter' => Config::get('DB_CONNECTION','mysql'),
+            'host' => Config::get('DB_HOST', 'mysql'),
+            'name' => Config::get('DB_DATABASE', 'app'),
             'user' => Config::get('DB_USERNAME', 'root'),
             'pass' => Config::get('DB_PASSWORD', 'root'),
             'port' => Config::get('DB_PORT', 3306),
             'charset' => 'utf8',
+        ],
+
+        'testing' => [
+            'adapter' => 'sqlite',
+            'name' => './database',
+            'suffix' => 'sqlite',
+            //'memory' => true,
         ],
     ],
     'version_order' => 'creation'
